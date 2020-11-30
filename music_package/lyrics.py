7e1be480b14f7cc959275145b35038124a1ae16f
businer <stefano.businaro@student.h-farm.com>
2020-11-30 11:46:29 +0100

    creation of a new module that manages the get_tabs_id function

--- music_package/lyrics.py
+++ music_package/lyrics.py
@@ -1,21 +1,21 @@
 import requests
 import json
 
+
+SONG_URL = 'https://api.lyrics.ovh/v1/{}/{}'
+
+
+def get_lyric( artist, title):
+
 """ 
 
 The function returns the lyrics of a song given the title and the artist.
-    
     :param artist: artist name
     :type artist: string
     :param title: title of the song
     :type title: string
 
 """
-
-SONG_URL = 'https://api.lyrics.ovh/v1/{}/{}'
-
-
-def get_lyric( artist, title):
 
     URL = SONG_URL.format(artist, title)
 
@@ -28,21 +28,3 @@ def get_lyric( artist, title):
         pass
 
     return song
-
-
-
-""" 
-
-The function returns the guitar tabs of a song.
-
-"""
-
-
-def get_tabs_id(artist):
-
-    r = requests.get('http://www.songsterr.com/a/ra/songs.json?pattern=' + artist)
-
-    my_dict = r.json()
-
-    return (my_dict, 'You will find the tabs of the song by googling http://www.songsterr.com/a/wa/song?id= and add the id of the song you want' )
-
--- music_package/tabs.py
+++ music_package/tabs.py
@@ -1,1 +1,13 @@
+def get_tabs_id(artist):
 
+""" 
+
+The function returns the guitar tabs of a song.
+
+"""
+
+    r = requests.get('http://www.songsterr.com/a/ra/songs.json?pattern=' + artist)
+
+    my_dict = r.json()
+
+    return (my_dict, 'You will find the tabs of the song by googling http://www.songsterr.com/a/wa/song?id= and add the id of the song you want' )
