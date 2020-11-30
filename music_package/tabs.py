ec65c84df85f081dd5e67a4c8ace055b6e5d2f8c
Matteo-Cobian <bianco.matteo2910@gmail.com>
2020-11-30 17:16:31 +0100

    Changed the tabs function (json.loads)

--- music_package/tabs.py
+++ music_package/tabs.py
@@ -8,6 +8,6 @@ The function returns the guitar tabs of a song.
 
     r = requests.get('http://www.songsterr.com/a/ra/songs.json?pattern=' + artist)
 
-    my_dict = r.json()
+    data = json.loads(r.text)
 
-    return (my_dict, 'You will find the tabs of the song by googling http://www.songsterr.com/a/wa/song?id= and add the id of the song you want' )
+    return (data, 'You will find the tabs of the song by googling http://www.songsterr.com/a/wa/song?id= and add the id of the song you want' )
