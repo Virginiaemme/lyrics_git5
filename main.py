08e864ebd9ba4e4ac837e00c22b1ba4cef6f2509
Federico <Gregofederico@gmail.com>
2020-11-30 12:11:01 +0100

    first lines of argparse function

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from music_package import get_lyric
+from music_package import lyrics.py
+from music_package import tabs.py
 
-artist = "Beatles"
-title= "Across the Universe"
-
-song = get_lyric(artist, title)
-
-print("{} by {}:".format(title, artist))
-print("{}".format(song))
+def argparse(): #here we collect the inputs, the results will be organized in arrays and inserted in the url
+    parser = argparse.ArgumentParser()
+    parser.add_argument("artist", help="add artist")# nargs='+')
+    parser.add_argument("title", help="add title", nargs='+')
+    parser.add_argument('-v','--verbosity',help='increase output verbosity', action='store_true')
+    args = parser.parse_args()
+    return args
