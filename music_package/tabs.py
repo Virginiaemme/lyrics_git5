7c9854fa87bcfc804e1e8fae8dc0e0f12bee73bd
businer <stefano.businaro@student.h-farm.com>
2020-12-02 17:30:55 +0100

    solved the problem of going in the next line in tabs.py

--- music_package/tabs.py
+++ music_package/tabs.py
@@ -8,7 +8,8 @@ def get_tabs_id(artist):
     r = requests.get('http://www.songsterr.com/a/ra/songs.json?pattern=' + artist)
 
     data = json.loads(r.text)
+    a = 'In order to get tabs of the song you have to put in the following link the ID of your song and google it. \n'
+    b = 'You can find the ID of the songs in the below database. \n\n'
+    c = 'LINK: http://www.songsterr.com/a/wa/song?id= \n\n'
 
-    return ('In order to get tabs of the song you have to put in the following link the ID of your song and google it.',
-    '\n', 'You can find the ID of the songs in the below database.', '\n\n',
-    'LINK: http://www.songsterr.com/a/wa/song?id=', '\n\n', data)
+    return (a, b, c, data)
