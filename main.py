ed6ecbb334a5a7f4b50c0e652904dce465c62a41
Federico <Gregofederico@gmail.com>
2020-12-02 15:25:06 +0100

    import sys added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from music_package import tabs as ta
 import argparse
 import os
 import databmanager
+import sys
 
 def argparse(): #here we collect the inputs, the results will be organized in arrays and inserted in the url
     parser = argparse.ArgumentParser()
