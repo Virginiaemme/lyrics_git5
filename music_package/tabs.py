47623dae5c46db1e49d7f10b9dde9c826b975c1f
businer <stefano.businaro@student.h-farm.com>
2020-12-02 16:56:43 +0100

    changed output

--- music_package/tabs.py
+++ music_package/tabs.py
@@ -3,14 +3,12 @@ import json
 
 def get_tabs_id(artist):
 
-    """ 
-
-    The function returns the guitar tabs of a song.
-
-    """
+    """  The function returns the guitar tabs of a song. """
 
     r = requests.get('http://www.songsterr.com/a/ra/songs.json?pattern=' + artist)
 
     data = json.loads(r.text)
 
-    return (data, 'You will find the tabs of the song by googling http://www.songsterr.com/a/wa/song?id= and add the id of the song you want' )
+    return ('In order to get tabs of the song you have to put in the following link the ID of your song and google it.',
+    '\n You can find the ID of the songs in the below database. \n\n',
+    'LINK: http://www.songsterr.com/a/wa/song?id=', '\n\n', data)
