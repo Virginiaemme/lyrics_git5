6fc1e6f541b9d3c9529a4169a328afe768496287
divi999 <francdivincenzo@gmail.com>
2020-12-02 14:46:38 +0100

    add verbosity to the lyrics file

--- music_package/lyrics.py
+++ music_package/lyrics.py
@@ -5,18 +5,18 @@ import json
 SONG_URL = 'https://api.lyrics.ovh/v1/{}/{}'
 
 
-def get_lyric( artist, title):
+def get_lyric( artist, title, verbosity=True):
 
-""" 
+    """ 
 
-The function returns the lyrics of a song given the title and the artist.
-    :param artist: artist name
-    :type artist: string
-    :param title: title of the song
-    :type title: string
+    The function returns the lyrics of a song given the title and the artist.
+        :param artist: artist name
+        :type artist: string
+        :param title: title of the song
+        :type title: string
 
-"""
 
+    """
     URL = SONG_URL.format(artist, title)
 
     r = requests.get(URL)
@@ -26,5 +26,8 @@ The function returns the lyrics of a song given the title and the artist.
         song = data['lyrics']
     except TypeError:
         pass
-
-    return song
+    
+    if verbosity:
+        return 'The song {} is from the artist {} and the lyrics is: {}'.format(title,artist,song)
+    else:
+        return song
