49d1cf3663eadabcf81ccaa57508ac99117dcd7f
businer <stefano.businaro@student.h-farm.com>
2020-12-04 14:52:16 +0100

    modified comments of lyrics.py and tabs.py accordingly to Sphinx

--- music_package/tabs.py
+++ music_package/tabs.py
@@ -2,15 +2,13 @@ import requests
 import json
 
 def get_tabs_id(artist):
+    """ The function returns the guitar tabs of a song.
 
-    """ 
-
-    The function returns the guitar tabs of a song.
-
+    :param artist: artist name
+    :type artist: string
+    :return: all the artist's songs information, the user have to pick the ID of the choosen song to insert in the provided link
+    :rtype: string
     """
-
     r = requests.get('http://www.songsterr.com/a/ra/songs.json?pattern=' + artist)
-
     data = json.loads(r.text)
-    
     return ("To get the tabs of the song insert the ID of the song in this link:  http://www.songsterr.com/a/wa/song?id=   ", data)
