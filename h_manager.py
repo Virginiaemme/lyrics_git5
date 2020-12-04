ff1c6babf2932b48c0a65452fcf70e9ed1369fd8
Federico <Gregofederico@gmail.com>
2020-12-04 18:46:11 +0100

    function for appending new lines in the csv

---
+++ h_manager.py
@@ -0,0 +1,9 @@
+from csv import DictWriter
+
+def append_dict_as_row(file_name, dict_of_name, field_name):
+    #copy in append mode
+    with open(file_name, 'a+', newline='') as write_obj:
+        # Create a writer object from csv module
+        dict_writer = DictWriter(write_obj, fieldnames=field_names)
+        # Add dictionary as wor in the csv
+        dict_writer.writerow(dict_of_elem)
