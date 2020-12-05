679ff51ebf08e335d5803672ead3657ea6573e49
Virginia <virginia.massaccesi@student.h-farm.com>
2020-12-05 16:16:42 +0100

    errata corige tabs.py

--- music_package/tabs.py
+++ music_package/tabs.py
@@ -3,20 +3,23 @@ import json
 
 def get_tabs_id(artist, verbosity=True):
 
-def get_tabs_id(artist):
+     """ 
 
-    """ 
+     The function returns the guitar tabs of a song.
 
-    The function returns the guitar tabs of a song.
+    :param artist: artist name
+    :type artist: string
+    :return: all the artist's songs information, the user have to pick the ID of the choosen song to insert in the provided link
+    :rtype: string
 
-    """ 
+    """
 
     r = requests.get('http://www.songsterr.com/a/ra/songs.json?pattern=' + artist)
     
     data = json.loads(r.text)
 
     link = 'To get the tabs insert the ID of the song after this link: http://www.songsterr.com/a/wa/song?id= \n\n'
-    b = 'Here you can find IDs of every song of {}, cheese the one that you are searching for here: \n\n {}'.format(artist,data)
+    b = 'Here you can find IDs of every song of {}, choose the one that you are searching for here: \n\n {}'.format(artist,data)
     if verbosity:
     	return (link + b)
     else:   
