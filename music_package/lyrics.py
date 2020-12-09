283974d0b3cfbfff411824d55a69bbe955d76273
divi999 <francdivincenzo@gmail.com>
2020-12-09 15:51:06 +0100

    obey to pep8 rules in lyrics

--- music_package/lyrics.py
+++ music_package/lyrics.py
@@ -1,22 +1,23 @@
 import requests
 import json
 
-def get_lyric( artist, title, verbosity=True):
+
+def get_lyric(artist, title, verbosity=True):
     """The function returns the lyrics of a song given the title and the artist.
-    
+
     :param artist: artist name
     :type artist: string
     :param title: title of the song
     :type title: string
-    :param verbosity: verbosity activated 
+    :param verbosity: verbosity activated
     :type verbosity: bool
-    :param SONG_URL: generic URL that will be implemented with input artist and title
+    :param SONG_URL: generic URL implemented with input artist and title
     :type SONG_URL: string
-    :return: lyrics of the chosen song 
-    :rtype: string 
-    
+    :return: lyrics of the chosen song
+    :rtype: string
+
     """
-   
+
     SONG_URL = 'https://api.lyrics.ovh/v1/{}/{}'
     URL = SONG_URL.format(artist, title)
     r = requests.get(URL)
@@ -24,8 +25,9 @@ def get_lyric( artist, title, verbosity=True):
     try:
         song = data['lyrics']
     except TypeError:
-        pass  
-    extend_output = 'The song {} is from the artist {} and the lyrics is:\n\n{}'.format(title, artist, song) 
+        pass
+    extend_output = 'The song {} is from the artist {}'.format(title, artist) \
+                    + ' and the lyrics is:\n\n{}'.format(song)
     if verbosity:
         return extend_output
     else:
