4e75635d3fb91a7d8e41996d7ea1db52d4e855c4
divi999 <francdivincenzo@gmail.com>
2020-12-09 20:05:42 +0100

    fixed error with the file loading

--- music_package/test/test_module.py
+++ music_package/test/test_module.py
@@ -1,5 +1,38 @@
 import unittest
-import sysimport os
-from music_package import lyrics
+import sys
+import os
+from music_package import lyrics as ly
+import json
+
+dictionary = {'Adele': 5}
 
 class TestInput(unittest.TestCase):
+
+    # smoke test: valid inputs
+    def test_correct_values(self):
+        # you should select some valid inputs, for which the output is known
+        self.assertEqual(ly.get_lyric('Descendents', 'All'), 'All!')
+
+    # corner case: empty string
+    def test_empty_string(self):
+        self.assertEqual(ly.get_lyric(" "," "), "")
+    
+    #dictionary = {'Adele': 5}
+    # invalid inputs
+    def test_wrong_values(self):
+        # you should input wrong data
+        self.assertEqual(ly.get_lyric(dictionary,dictionary), None)
+
+        # NOTE: the following test passing an empty list will fail!
+        # self.assertEqual(return_birthday([]), None)
+
+
+        
+        
+if __name__ == "__main__":
+    
+    # basic test
+    #unittest.main()
+
+    # with more details
+    unittest.main(verbosity=False)
