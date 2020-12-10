0ec884718fcd1d383b7db6d1645da2a7a93f8c9b
Matteo-Cobian <bianco.matteo2910@gmail.com>
2020-12-10 14:16:47 +0100

    removed not needed files

--- h_manager.py
+++
@@ -1,9 +0,0 @@
-from csv import DictWriter
-
-def append_dict_as_row(file_name, dict_of_name, field_name):
-    #copy in append mode
-    with open(file_name, 'a+', newline='') as write_obj:
-        # Create a writer object from csv module
-        dict_writer = DictWriter(write_obj, fieldnames=field_name)
-        # Add dictionary as in the csv
-        dict_writer.writerow(dict_of_name)
